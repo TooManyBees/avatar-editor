e01619f64c0d612ab5fecd761a81c8fc520841c9
Jess Bees <hi@toomanybees.com>
2021-12-30 14:06:19 -0500

    Sync textarea height with preview

--- src/ui/components/TextArea.tsx
+++ src/ui/components/TextArea.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import elementsFromColorCodes from "./elementsFromColorCodes";
 import styles from "./TextArea.module.css";
 
@@ -12,6 +12,7 @@ interface TextFieldProps {
 export default function TextArea(props: TextFieldProps) {
 	const [value, setState] = useState(props.value);
 	const [preview, setPreview] = useState(false);
+	const textArea = useRef<HTMLTextAreaElement>(null);
 
 	function onChange(event: ChangeEvent<HTMLTextAreaElement>) {
 		setState(event.currentTarget.value);
@@ -26,8 +27,7 @@ export default function TextArea(props: TextFieldProps) {
 	}
 
 	const name = props.name;
-	const valueString = value == null ? "" : value.toString();
-	const rows = (valueString.match(/\n/g)?.length || 0) + 1;
+	const rows = (value.match(/\n/g)?.length || 0) + 1;
 	return (
 		<label className={styles.textArea}>
 			<span className={styles.description}>
@@ -35,8 +35,8 @@ export default function TextArea(props: TextFieldProps) {
 				<label><input type="checkbox" checked={preview} onChange={togglePreview} /> Preview colors</label>
 			</span>
 			{ preview
-				? <div className={styles.preview}>{elementsFromColorCodes(value)}</div>
-				: <textarea rows={rows} value={valueString} onChange={onChange} onBlur={onBlur} />
+				? <div className={styles.preview} style={{height: textArea.current?.offsetHeight}}>{elementsFromColorCodes(value)}</div>
+				: <textarea rows={rows} value={value} onChange={onChange} onBlur={onBlur} ref={textArea} />
 			}
 		</label>
 	);
