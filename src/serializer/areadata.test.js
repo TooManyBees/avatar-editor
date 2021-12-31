3c25a20969408ef430b6c2de221176b87bbac26a
Jess Bees <hi@toomanybees.com>
2021-12-31 13:52:24 -0500

    serialize areadata

---
+++ src/serializer/areadata.test.js
@@ -0,0 +1,49 @@
+import parseAreadata from "../parser/areadata";
+import serializeAreadata from "./areadata";
+
+describe(`serializeAreadata`, () => {
+	const AREADATA = `#AREADATA
+P 2 1
+F 1|2
+O 101 102 103 104 105
+K 1 2 100 150 text~
+M 11 12 13 14 15 16 17 18
+G 11 12 13 14 15 16 17 18
+V 100 199
+B 10000 100000
+S
+`
+
+	it("serializes all of the areadata lines", () => {
+		const state = parseState(AREADATA);
+		expect(serializeAreadata(state)).toBe(AREADATA);
+	});
+
+	it("serializes a plane without a zone", () => {
+		const AREADATA_WITHOUT_ZONE = `#AREADATA
+P 1
+S
+`;
+		const state = parseState(AREADATA_WITHOUT_ZONE);
+		expect(serializeAreadata(state)).toBe(AREADATA_WITHOUT_ZONE);
+	});
+
+	it("omits areadata when it is empty", () => {
+		const state = parseState("#AREADATA\nS\n");
+		expect(serializeAreadata(state)).toBe("");
+	});
+});
+
+function parseState(string) {
+	// Trim off the #AREADATA\n
+	const areadata = parseAreadata(string.substring(10));
+	const areadataBits = {};
+	for (let key of Object.keys(areadata)) {
+		areadataBits[key] = true;
+	}
+	const state = {
+		...areadata,
+		areadataBits,
+	};
+	return state;
+}
--- src/serializer/areadata.ts
+++ src/serializer/areadata.ts
@@ -3,5 +3,38 @@ import { AreadataState } from "../app/store/areadata";
 export default function serializeAreadata(state: AreadataState): string {
 	let buffer = "";
 
-	return buffer;
+	if (state.areadataBits.plane && state.plane) {
+		if (state.plane.zone)
+			buffer += `P ${state.plane.plane} ${state.plane.zone}\n`;
+		else
+			buffer += `P ${state.plane.plane}\n`;
+	}
+	if (state.areadataBits.flags && state.flags) {
+		buffer += `F ${state.flags.flags.join('|')}\n`;
+	}
+	if (state.areadataBits.outlaw && state.outlaw) {
+		let { dumpVnum, jailVnum, deathVnum, execVnum, justice } = state.outlaw;
+		buffer += `O ${dumpVnum} ${jailVnum} ${deathVnum} ${execVnum} ${justice}\n`;
+	}
+	if (state.areadataBits.kspawn && state.kspawn) {
+		let { condition, command, mobVnum, roomVnum, text } = state.kspawn;
+		buffer += `K ${condition} ${command} ${mobVnum} ${roomVnum} ${text}~\n`;
+	}
+	if (state.areadataBits.modifier && state.modifier) {
+		const { xpGain, hpRegen, manaRegen, moveRegen, statloss, respawnRoom, tbd1, tbd2 } = state.modifier;
+		buffer += `M ${xpGain} ${hpRegen} ${manaRegen} ${moveRegen} ${statloss} ${respawnRoom} ${tbd1} ${tbd2}\n`;
+	}
+	if (state.areadataBits.groupSize && state.groupSize) {
+		const { pct0, num1, pct1, num2, pct2, pct3, div, tbd } = state.groupSize;
+		buffer += `G ${pct0} ${num1} ${pct1} ${num2} ${pct2} ${pct3} ${div} ${tbd}\n`;
+	}
+	if (state.areadataBits.vnumRange && state.vnumRange) {
+		buffer += `V ${state.vnumRange.min} ${state.vnumRange.max}\n`;
+	}
+	if (state.areadataBits.scaling && state.scaling) {
+		buffer += `B ${state.scaling.maxGroupPower} ${state.scaling.maxGroupToughness}\n`;
+	}
+
+	if (buffer !== "") return `#AREADATA\n${buffer}S\n`;
+	else return "";
 }
