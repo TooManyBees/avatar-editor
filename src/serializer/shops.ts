adb5f45274fc32b6c63d573af172dd5acd69ad7d
Jess Bees <hi@toomanybees.com>
2021-12-31 23:56:28 -0500

    serialize shops

--- src/serializer/shops.ts
+++ src/serializer/shops.ts
@@ -1,9 +1,25 @@
 import { Mobile, Shop } from "../app/models";
+import { findVnum } from "../app/models/helpers";
 
 type Shops = { [s: string]: Shop };
 
 export default function serializeShops(shops: Shops, mobiles: Mobile[]): string {
 	let buffer = "";
 
-	return buffer;
+	for (let shop of Object.values(shops)) {
+		buffer += serializeShop(shop, mobiles) + "\n";
+	}
+
+	if (buffer !== "") return `#SHOPS\n\n${buffer}0\n`;
+	else return "";
+}
+
+export function serializeShop(shop: Shop, mobiles: Mobile[]): string {
+	let vnum = findVnum(mobiles, shop.mobId);
+
+	return `${vnum}
+${shop.oType1} ${shop.oType2} ${shop.oType3} ${shop.oType4} ${shop.oType5}
+${shop.profitBuy} ${shop.profitSell}
+${shop.firstHour} ${shop.lastHour}
+`;
 }
