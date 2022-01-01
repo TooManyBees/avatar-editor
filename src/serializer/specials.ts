50a1824b219668713c915785dc6fa8c8a7b27dad
Jess Bees <hi@toomanybees.com>
2022-01-01 09:54:56 -0500

    serialize specials

--- src/serializer/specials.ts
+++ src/serializer/specials.ts
@@ -1,9 +1,16 @@
 import { Mobile } from "../app/models";
+import { findVnum } from "../app/models/helpers";
 
 type Specials = { [s: string]: string };
 
 export default function serializeSpecials(specials: Specials, mobiles: Mobile[]): string {
 	let buffer = "";
 
-	return buffer;
+	for (let [mobId, specFun] of Object.entries(specials)) {
+		let vnum = findVnum(mobiles, mobId);
+		buffer += `M ${vnum} ${specFun}\n`;
+	}
+
+	if (buffer !== "") return `#SPECIALS\n${buffer}S\n`;
+	else return "";
 }
