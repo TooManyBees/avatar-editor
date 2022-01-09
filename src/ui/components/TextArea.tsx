8b7b8ed2286ad4dbf049f42d7fd0d48a2cfd93a8
Jess Bees <hi@toomanybees.com>
2022-01-09 14:57:19 -0500

    Link textarea's label to its input

--- src/ui/components/TextArea.tsx
+++ src/ui/components/TextArea.tsx
@@ -13,12 +13,15 @@ interface TextFieldProps {
 	colors?: ColorScheme;
 }
 
+let iteration = 0;
+
 export default function TextArea(props: TextFieldProps) {
 	const [value, setState] = useState(props.value);
 	const [preview, setPreview] = useState(false);
 	const [height, setHeight] = useState<number | undefined>(undefined);
 	const [width, setWidth] = useState<number | undefined>(undefined);
 	const textArea = useRef<HTMLTextAreaElement>(null);
+	const nthTextArea = useRef<number>(iteration++);
 
 	function onChange(event: ChangeEvent<HTMLTextAreaElement>) {
 		setState(event.currentTarget.value);
@@ -36,19 +39,20 @@ export default function TextArea(props: TextFieldProps) {
 		setPreview(event.target.checked);
 	}
 
+	const textAreaId = `textarea-${nthTextArea.current}`;
 	const name = props.name;
 	const rows = (value.match(/\n/g)?.length || 0) + 1;
 	return (
 		<div className={styles.textArea}>
 			<span className={styles.description}>
 				{/* FIXME: apply htmlFor pointing to the textarea */}
-				<label>{name}:</label>
+				<label htmlFor={textAreaId}>{name}:</label>
 				<label><input type="checkbox" checked={preview} onChange={togglePreview} /> Preview colors</label>
 			</span>
 			<div className={styles.editor}>
 				{ preview
 					? <pre className={classnames(styles.colorPreview, props.colors && styles[props.colors])} style={{height, width}}>{elementsFromColorCodes(value)}</pre>
-					: <textarea rows={rows} cols={90} style={{height, width}} value={value} onChange={onChange} onBlur={onBlur} ref={textArea} />
+					: <textarea id={textAreaId} rows={rows} cols={90} style={{height, width}} value={value} onChange={onChange} onBlur={onBlur} ref={textArea} />
 				}
 			</div>
 		</div>
